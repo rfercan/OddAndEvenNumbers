a3c29f3f7b28e57e60f8852059c94981f1fa818a
Fatih <r.fatihercan@hotmail.com>
2022-11-20 08:59:52 +0300

    Add project files.

---
+++ OddAndEvenNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 1; i <= 10; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i + " is even number");
+
+    }
+    else
+    {
+        Console.WriteLine(i + " is odd number ");
+    }
+}
